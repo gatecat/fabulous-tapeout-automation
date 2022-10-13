81ed7fc08e32892d576359720b5468ce2038d7aa
gatecat <gatecat@ds0.me>
2022-10-13 12:05:32 +0200

    add fabric.csv parser
    Signed-off-by: gatecat <gatecat@ds0.me>

---
+++ fab_automation/util/fabric_csv.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+@dataclass
+class FabricTile:
+    tiletype: str
+    bel_verilog: set[str] = field(default_factory=set)
+
+@dataclass
+class SuperTile:
+    tiletype: str
+    subtiles: list[list[str]]
+
+@dataclass
+class FabricCsv:
+    tiletypes: dict[str, FabricTile] = field(default_factory=dict)
+    supertiles: dict[str, SuperTile] = field(default_factory=dict)
+    tilegrid: list[list[str]] = field(default_factory=list)
+
+    def parse(filename):
+        result = FabricCsv()
+        lines = []
+        with open(filename, "r") as f:
+            for line in f:
+                line = line.split('#')[0].strip().split(",")
+                if len(line) == 0:
+                    continue
+                lines.append(line)
+        lines.reverse() # so we can 'pop'
+
+        def parse_grid(end_tok):
+            grid = []
+            while lines:
+                l = lines.pop()
+                if l[0] == end_tok:
+                    break
+                tile_line = []
+                for t in l:
+                    if t == "":
+                        break
+                    tile_line.append(t)
+                if len(tile_line) > 0:
+                    grid.append(tile_line)
+            return grid
+
+        while lines:
+            l = lines.pop()
+            cmd = l[0]
+            if cmd == "":
+                continue
+            elif cmd == "FabricBegin":
+                # parse tilegrid
+                result.tilegrid = parse_grid("FabricEnd")
+            elif cmd == "TILE":
+                tiletype = l[1]
+                tile = FabricTile(tiletype=tiletype)
+                while lines:
+                    l = lines.pop()
+                    if l[0] == "EndTILE":
+                        break
+                    if l[0] == "BEL":
+                        tile.bel_verilog.add(l[1].replace(".vhdl", ".v")) # ...?
+                result.tiletypes[tiletype] = tile
+            elif cmd == "SuperTILE":
+                tiletype = l[1]
+                tile = SuperTile(tiletype=tiletype, subtiles=parse_grid("EndSuperTILE"))
+                result.supertiles[tiletype] = tile
+        return result
+
+if __name__ == '__main__':
+    import sys, pprint
+    pprint.pprint(FabricCsv.parse(sys.argv[1]))
--- fab_automation/util/project.py
+++ fab_automation/util/project.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass, field, fields
 from typing import Optional
 
-__all__ = ["TileType", "PinConfig", "FabricConfig", "Project"]
+__all__ = ["PhysTileType", "PinConfig", "FabricConfig", "TapepoutProject"]
 
 @dataclass
-class TileType:
+class PhysTileType:
     tiletype: str
     width: int
     height: int
@@ -29,6 +29,11 @@ class FabricConfig:
     edge_space_s: int = 0
     edge_space_w: int = 0
 
+@dataclass
+class FabulousIntegration
+    fabric_csv: str
+    verilog_root: str
+
 def _parse_section(sec_class, lines, init_args=dict()):
     args = dict()
     field_types = {field.name: field.type for field in fields(sec_class)}
@@ -47,14 +52,15 @@ def _parse_section(sec_class, lines, init_args=dict()):
     return sec_class(**init_args, **args)
 
 @dataclass
-class Project:
-    tiles: dict[str, TileType] = field(default_factory=dict)
+class TapepoutProject:
+    tiles: dict[str, PhysTileType] = field(default_factory=dict)
     pin_cfg: Optional[PinConfig] = None
-    fab_cfg: Optional[PinConfig] = None
+    fab_cfg: Optional[FabricConfig] = None
+    fabulous: Optional[FabulousIntegration] = None
 
     def parse(filename):
         lines = []
-        result = Project()
+        result = TapepoutProject()
         with open(filename, "r") as f:
             for line in f:
                 line = line.split('#')[0].strip()
@@ -69,16 +75,18 @@ class Project:
                 continue
             cmd = sl[0].lower()
             if cmd == "tile":
-                tile = _parse_section(TileType, lines, init_args=dict(tiletype=sl[1]))
+                tile = _parse_section(PhysTileType, lines, init_args=dict(tiletype=sl[1]))
                 result.tiles[tile.tiletype] = tile
             elif cmd == "pin_config":
                 result.pin_cfg = _parse_section(PinConfig, lines)
             elif cmd == "fab_config":
                 result.fab_cfg = _parse_section(FabricConfig, lines)
+            elif cmd == "fabulous":
+                result.fabulous = _parse_section(FabulousIntegration, lines)
             else:
                 assert False, f"unknown command {cmd}"
         return result
 
 if __name__ == '__main__':
     import sys, pprint
-    pprint.pprint(Project.parse(sys.argv[1]))
+    pprint.pprint(TapepoutProject.parse(sys.argv[1]))
